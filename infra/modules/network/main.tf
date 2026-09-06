27c2416db7777cd4255f90b393b4c6238d69eb50
Sandeep Pandit <24panditsandeep@gmail.com>
2026-09-06 18:29:47 +0000

    Simplify private network route

--- infra/modules/network/main.tf
+++ infra/modules/network/main.tf
@@ -120,21 +120,20 @@ resource "aws_nat_gateway" "this" {
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.this.id
 
-  dynamic "route" {
-    for_each = var.enable_nat_gateway ? [1] : []
-
-    content {
-      cidr_block     = "0.0.0.0/0"
-      nat_gateway_id = aws_nat_gateway.this[0].id
-    }
-  }
-
   tags = merge(
     local.common_tags,
     {
       Name = "${var.project_name}-${var.environment}-private-rt"
     }
   )
+}
+
+resource "aws_route" "private_nat" {
+  count = var.enable_nat_gateway ? 1 : 0
+
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.this[0].id
 }
 
 resource "aws_route_table_association" "private" {
